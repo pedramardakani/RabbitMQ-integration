ef58e7f65fb5467eb9961e160472d7821501346b
Pedram Ashofteh Ardakani <pedramardakani@pm.me>
2022-09-14 13:42:15 +0430

    Python (receive.py): close connection gracefully
    Until now, we'd just interrupt the main loop which unexpectedly closed the
    connection to the RabbitMQ instance.
    
    With this commit, we close the connection before exiting the program.

--- python_app/receive.py
+++ python_app/receive.py
@@ -3,43 +3,51 @@ from os import getenv
 from os import _exit as os_exit
 from sys import exit as sys_exit
 
-def main():
+# Connection params
+#
+# If running from a docker compose, use the environment variables
+# given, otherwise, define them with the default values.
+host     : str = getenv('CUSTOM_HOSTNAME',    'localhost')
+port     : int = getenv('RABBITMQ_NODE_PORT',        5672)
+vhost    : str = getenv('RBMQ_VHOST',                 '/')
+username : str = getenv('RBMQ_USERNAME',          'guest')
+password : str = getenv('RBMQ_PASSWORD',          'guest')
+qname    : str = getenv('RBMQ_QUEUE',              'test')
 
-    # Connection params
-    #
-    # If running from a docker compose, use the environment variables
-    # given, otherwise, define them with the default values.
-    host     : str = getenv('CUSTOM_HOSTNAME',    'localhost')
-    port     : int = getenv('RABBITMQ_NODE_PORT',        5672)
-    vhost    : str = getenv('RBMQ_VHOST',                 '/')
-    username : str = getenv('RBMQ_USERNAME',          'guest')
-    password : str = getenv('RBMQ_PASSWORD',          'guest')
-    qname    : str = getenv('RBMQ_QUEUE',              'test')
 
+def create_connection() -> pika.BlockingConnection:
 
     # Initialize
     credentials = pika.PlainCredentials(username, password)
     parameters = pika.ConnectionParameters(host, port, vhost, credentials)
     connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(qname)
 
+    return connection
+
+
+def main(connection: pika.BlockingConnection):
+
+    channel = connection.channel()
 
     # Logic
     def callback(ch, method, props, body: bytes) -> None:
         print(f">>> Received: '{body}'")
 
+    channel.queue_declare(qname)
     channel.basic_consume(queue=qname, auto_ack=True, on_message_callback=callback)
-
     channel.start_consuming()
 
 
 # Execute and graceful exit
 if __name__ == '__main__':
+
+    connection = create_connection()
+
     try:
-        main()
+        main(connection)
     except KeyboardInterrupt:
-        print('Interrupted')
+        print('\nWARNING: interrupted\n')
+        connection.close()
         try:
             sys_exit(0)
         except:
